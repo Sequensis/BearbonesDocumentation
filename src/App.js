45e1cd3d087b76777a1573e3b05afdd65a10155e
Humenko <danielhumenko@gmail.com>
2018-02-11 23:52:35 +0000

    add 404 page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Navbar from './components/Shared/Navbar'
 import Footer from './components/Shared/Footer'
 import Home from './components/Home/Home'
 import About from './components/About/About'
 import Documentation from './components/Documentation/Documentation'
+import PageNotFound from './components/Error/PageNotFound'
 
 class App extends Component {
   render() {
     return (
       <div>
         <Navbar />
-        
-        <Route exact path="/" component={Home}/>
-        <Route path="/about" component={About}/>
-        <Route path="/documentation" component={Documentation}/>
-
+          <Switch>
+            <Route exact path="/home" component={Home}/>
+            <Route path="/about" component={About}/>
+            <Route path="/documentation" component={Documentation}/>
+            <Route component={PageNotFound} />
+          </Switch>
         <Footer />
 
       </div>
