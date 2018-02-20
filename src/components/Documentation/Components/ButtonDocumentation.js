37fb126e5abfe302f35b9675d6778cb4ae36c18f
Humenko <danielhumenko@gmail.com>
2018-02-20 23:19:29 +0000

    add button docs

--- src/components/Documentation/Components/ButtonDocumentation.js
+++ src/components/Documentation/Components/ButtonDocumentation.js
@@ -7,16 +7,16 @@ class ButtonDocumentation extends Component {
 			<div className="section">
 				<div className="container">
 					<div className="bb-grid md">
-						<div className="column column-sm">
+						<div className="column">
 							<h2 className="title">Button</h2>
 							<p>
-								An essential element of any design, providing the user a simple
-								way to trigger an event.
+								An essential element for any design, providing the user a simple way to trigger an
+								event.
 							</p>
 						</div>
 					</div>
 					<div className="bb-grid md">
-						<div className="column column-sm">
+						<div className="column">
 							<h4 className="title">Basic</h4>
 							<div className="bb-box example">
 								<div className="doc-preview">
@@ -25,12 +25,8 @@ class ButtonDocumentation extends Component {
 									<input className="bb-btn" type="submit" value="Input" />
 								</div>
 								<div className="doc-snippet">
-									<Highlight className="html">
-										{'<button class="bb-btn">Button</button>'}
-									</Highlight>
-									<Highlight className="html">
-										{'<a class="bb-btn">Anchor</a>'}
-									</Highlight>
+									<Highlight className="html">{'<button class="bb-btn">Button</button>'}</Highlight>
+									<Highlight className="html">{'<a class="bb-btn">Anchor</a>'}</Highlight>
 									<Highlight className="html">
 										{'<input class="bb-btn" type="submit" value="Input" />'}
 									</Highlight>
@@ -40,7 +36,7 @@ class ButtonDocumentation extends Component {
 						</div>
 					</div>
 					<div className="bb-grid md">
-						<div className="column column-sm">
+						<div className="column">
 							<h4 className="title">Colours</h4>
 							<div className="bb-box example">
 								<div className="doc-preview">
@@ -72,7 +68,7 @@ class ButtonDocumentation extends Component {
 						</div>
 					</div>
 					<div className="bb-grid md">
-						<div className="column column-sm">
+						<div className="column">
 							<h4 className="title">Sizes</h4>
 							<div className="bb-box example">
 								<div className="doc-preview">
@@ -89,9 +85,7 @@ class ButtonDocumentation extends Component {
 									<Highlight className="html">
 										{'<button class="bb-btn small">Small</button>'}
 									</Highlight>
-									<Highlight className="html">
-										{'<button class="bb-btn">Medium</button>'}
-									</Highlight>
+									<Highlight className="html">{'<button class="bb-btn">Medium</button>'}</Highlight>
 									<Highlight className="html">
 										{'<button class="bb-btn lg">Large</button>'}
 									</Highlight>
@@ -101,6 +95,231 @@ class ButtonDocumentation extends Component {
 									<button className="bb-btn primary doc-copy sm">Copy</button>
 								</div>
 							</div>
+						</div>
+					</div>
+					<div className="bb-grid md">
+						<div className="column">
+							<h4 className="title">Styles</h4>
+							<div className="bb-box example">
+								<div className="doc-preview">
+									<button className="bb-btn outline">Default</button>
+									<button className="bb-btn outline primary m-x-2">Primary</button>
+									<button className="bb-btn outline secondary">Secondary</button>
+									<button className="bb-btn outline positive m-x-2">Positive</button>
+									<button className="bb-btn outline negative">Negative</button>
+									<button className="bb-btn outline invert m-l-2">Invert</button>
+								</div>
+								<div className="doc-snippet">
+									<Highlight className="html">
+										{'<button class="bb-btn outline">Default</button>'}
+									</Highlight>
+									<Highlight className="html">
+										{'<button class="bb-btn outline primary">Primary</button>'}
+									</Highlight>
+									<Highlight className="html">
+										{'<button class="bb-btn outline secondary">Secondary</button>'}
+									</Highlight>
+									<Highlight className="html">
+										{'<button class="bb-btn outline positive">Positive</button>'}
+									</Highlight>
+									<Highlight className="html">
+										{'<button class="bb-btn outline negative">Negative</button>'}
+									</Highlight>
+									<Highlight className="html">
+										{'<button class="bb-btn outline invert">Invert</button>'}
+									</Highlight>
+									<button className="bb-btn primary doc-copy sm">Copy</button>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="bb-grid md">
+						<div className="column">
+							<div className="bb-box example">
+								<div className="doc-preview">
+									<button className="bb-btn inverted">Default</button>
+									<button className="bb-btn inverted primary m-x-2">Primary</button>
+									<button className="bb-btn inverted secondary">Secondary</button>
+									<button className="bb-btn inverted positive m-x-2">Positive</button>
+									<button className="bb-btn inverted negative">Negative</button>
+									<button className="bb-btn inverted invert m-l-2">Invert</button>
+								</div>
+								<div className="doc-snippet">
+									<Highlight className="html">
+										{'<button class="bb-btn inverted">Default</button>'}
+									</Highlight>
+									<Highlight className="html">
+										{'<button class="bb-btn inverted primary">Primary</button>'}
+									</Highlight>
+									<Highlight className="html">
+										{'<button class="bb-btn inverted secondary">Secondary</button>'}
+									</Highlight>
+									<Highlight className="html">
+										{'<button class="bb-btn inverted positive">Positive</button>'}
+									</Highlight>
+									<Highlight className="html">
+										{'<button class="bb-btn inverted negative">Negative</button>'}
+									</Highlight>
+									<Highlight className="html">
+										{'<button class="bb-btn inverted invert">Invert</button>'}
+									</Highlight>
+									<button className="bb-btn primary doc-copy sm">Copy</button>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="bb-grid md">
+						<div className="column">
+							<h4 className="title">State</h4>
+							<div className="bb-box example">
+								<div className="doc-preview">
+									<button className="bb-btn loading">Loading</button>
+									<button className="bb-btn loading primary m-x-2">Loading</button>
+									<button className="bb-btn loading secondary">Loading</button>
+									<button className="bb-btn loading positive m-x-2">Loading</button>
+									<button className="bb-btn loading negative">Loading</button>
+									<button className="bb-btn loading invert m-l-2">Loading</button>
+									<br />
+									<br />
+									<button className="bb-btn loading outline">Loading</button>
+									<button className="bb-btn loading outline primary m-x-2">Loading</button>
+									<button className="bb-btn loading outline secondary">Loading</button>
+									<button className="bb-btn loading outline positive m-x-2">Loading</button>
+									<button className="bb-btn loading outline negative">Loading</button>
+									<button className="bb-btn loading outline invert m-l-2">Loading</button>
+								</div>
+								<div className="doc-snippet">
+									<Highlight className="html">
+										{'<button class="bb-btn loading">Loading</button>'}
+									</Highlight>
+									<button className="bb-btn primary doc-copy sm">Copy</button>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="bb-grid md">
+						<div className="column">
+							<div className="bb-box example">
+								<div className="doc-preview">
+									<button disabled className="bb-btn disabled">
+										Disabled
+									</button>
+									<button disabled className="bb-btn disabled primary m-x-2">
+										Disabled
+									</button>
+									<button disabled className="bb-btn disabled secondary">
+										Disabled
+									</button>
+									<button disabled className="bb-btn disabled positive m-x-2">
+										Disabled
+									</button>
+									<button disabled className="bb-btn disabled negative">
+										Disabled
+									</button>
+									<button disabled className="bb-btn disabled invert m-l-2">
+										Disabled
+									</button>
+									<br />
+									<br />
+									<button disabled className="bb-btn disabled outline">
+										Disabled
+									</button>
+									<button disabled className="bb-btn disabled outline primary m-x-2">
+										Disabled
+									</button>
+									<button disabled className="bb-btn disabled outline secondary">
+										Disabled
+									</button>
+									<button disabled className="bb-btn disabled outline positive m-x-2">
+										Disabled
+									</button>
+									<button disabled className="bb-btn disabled outline negative">
+										Disabled
+									</button>
+									<button disabled className="bb-btn disabled outline invert m-l-2">
+										Disabled
+									</button>
+								</div>
+								<div className="doc-snippet">
+									<Highlight className="html">
+										{'<button class="bb-btn disabled">Disabled</button>'}
+									</Highlight>
+									<Highlight className="html">
+										{'<button disabled class="bb-btn">Disabled</button>'}
+									</Highlight>
+									<button className="bb-btn primary doc-copy sm">Copy</button>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="bb-grid md">
+						<div className="column">
+							<div className="bb-box example">
+								<div className="doc-preview">
+									<button className="bb-btn active">Active</button>
+									<button className="bb-btn active primary m-x-2">Active</button>
+									<button className="bb-btn active secondary">Active</button>
+									<button className="bb-btn active positive m-x-2">Active</button>
+									<button className="bb-btn active negative">Active</button>
+									<button className="bb-btn active invert m-l-2">Active</button>
+									<br />
+									<br />
+									<button className="bb-btn active outline">Active</button>
+									<button className="bb-btn active outline primary m-x-2">Active</button>
+									<button className="bb-btn active outline secondary">Active</button>
+									<button className="bb-btn active outline positive m-x-2">Active</button>
+									<button className="bb-btn active outline negative">Active</button>
+									<button className="bb-btn active outline invert m-l-2">Active</button>
+								</div>
+								<div className="doc-snippet">
+									<Highlight className="html">
+										{'<button class="bb-btn active">Active</button>'}
+									</Highlight>
+									<button className="bb-btn primary doc-copy sm">Copy</button>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="bb-grid md">
+						<div className="column">
+							<h4 className="title">Helper</h4>
+							<div className="bb-box example">
+								<div className="doc-preview">
+									<button className="bb-btn full-x">Full width</button>
+									<br />
+									<button className="bb-btn no-border-radius">No border radius</button>
+								</div>
+								<div className="doc-snippet">
+									<Highlight className="html">
+										{'<button className="bb-btn full-x">Full width</button>'}
+									</Highlight>
+									<Highlight className="html">
+										{'<button className="bb-btn no-border-radius">No border radius</button>'}
+									</Highlight>
+
+									<button className="bb-btn primary doc-copy sm">Copy</button>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="bb-grid md">
+						<div className="column">
+							<div className="bb-box example">
+								<div className="doc-preview">
+									<a className="link">Link</a>
+									<a className="link primary m-x-2">Link</a>
+									<a className="link secondary">Link</a>
+									<a className="link positive m-x-2">Link</a>
+									<a className="link negative">Link</a>
+									<a className="link invert m-l-2">Link</a>
+								</div>
+								<div className="doc-snippet">
+									<Highlight className="html">{'<a class="link">Link</a>'}</Highlight>
+
+									<button className="bb-btn primary doc-copy sm">Copy</button>
+								</div>
+							</div>
+							<h4 className="title">todo: icons / close / button group</h4>
 						</div>
 					</div>
 				</div>
