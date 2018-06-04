7a0f379540ea2fa13575bf8ed4454696b550a1ab
Humenko <danielhumenko@gmail.com>
2018-06-04 11:00:19 +0100

    Create breadcrumb doc

--- src/components/Documentation/Components/BreadcrumbDocumentation.jsx
+++ src/components/Documentation/Components/BreadcrumbDocumentation.jsx
@@ -22,31 +22,35 @@ export default class BreadcrumbDocumentation extends Component {
               <h4 className="title">Basic</h4>
               <div className="bb-box example">
                 <div className="doc-preview">
-                  <div className="content">
-                    <div className="column column-10">
-                      <div className="bb-box">
-                        <h4 className="title">
-                          The giant panda is actually a bear.
-                        </h4>
-                        <p className="subtitle">
-                          The giant panda also known as panda bear or simply
-                          panda, is a bear native to south central China.
-                        </p>
-                      </div>
-                    </div>
-                  </div>
+                  <nav className="bb-breadcrumb">
+                    <ul>
+                      <li className="breadcrumb-item active">
+                        <a href="#example">Carnivorans</a>
+                      </li>
+                      <li className="breadcrumb-item">
+                        <a href="#example">Ursidae</a>
+                      </li>
+                      <li className="breadcrumb-item">
+                        <a href="#example">Panda</a>
+                      </li>
+                    </ul>
+                  </nav>
                 </div>
                 <div className="doc-snippet">
                   <Highlight className="html">
-                    {`<div className="bb-box">
-  <h4 className="title">
-    The giant panda is actually a bear.
-  </h4>
-  <p className="subtitle">
-    The giant panda also known as panda bear or simply
-    panda, is a bear native to south central China.
-  </p>
-</div>`}
+                    {`<nav className="bb-breadcrumb">
+  <ul>
+    <li className="breadcrumb-item active">
+      <a href="#example">Carnivorans</a>
+    </li>
+    <li className="breadcrumb-item">
+      <a href="#example">Ursidae</a>
+    </li>
+    <li className="breadcrumb-item">
+      <a href="#example">Panda</a>
+    </li>
+  </ul>
+</nav>`}
                   </Highlight>
                   <button className="bb-btn primary doc-copy sm">Copy</button>
                 </div>
@@ -55,66 +59,125 @@ export default class BreadcrumbDocumentation extends Component {
           </div>
           <div className="bb-grid md">
             <div className="column column-16">
-              <h4 className="title">Colours</h4>
+              <h4 className="title">Sizes</h4>
               <div className="bb-box example">
                 <div className="doc-preview">
-                  <div className="column column-10">
-                    <div className="bb-box invert">
-                      <h4>The giant panda is actually a bear.</h4>
-                    </div>
-                  </div>
-                  <div className="column column-10">
-                    <div className="bb-box primary">
-                      <h4>The giant panda is actually a bear.</h4>
-                    </div>
-                  </div>
-                  <div className="column column-10">
-                    <div className="bb-box secondary">
-                      <h4>The giant panda is actually a bear.</h4>
-                    </div>
-                  </div>
-                  <div className="column column-10">
-                    <div className="bb-box positive">
-                      <h4>The giant panda is actually a bear.</h4>
-                    </div>
-                  </div>
-                  <div className="column column-10">
-                    <div className="bb-box negative">
-                      <h4>The giant panda is actually a bear.</h4>
-                    </div>
-                  </div>
+                  <nav className="bb-breadcrumb xs">
+                    <ul>
+                      <li className="breadcrumb-item active">
+                        <a href="#example">Carnivorans</a>
+                      </li>
+                      <li className="breadcrumb-item">
+                        <a href="#example">Ursidae</a>
+                      </li>
+                      <li className="breadcrumb-item">
+                        <a href="#example">Panda</a>
+                      </li>
+                    </ul>
+                  </nav>
+                  <nav className="bb-breadcrumb sm">
+                    <ul>
+                      <li className="breadcrumb-item active">
+                        <a href="#example">Carnivorans</a>
+                      </li>
+                      <li className="breadcrumb-item">
+                        <a href="#example">Ursidae</a>
+                      </li>
+                      <li className="breadcrumb-item">
+                        <a href="#example">Panda</a>
+                      </li>
+                    </ul>
+                  </nav>
+                  <nav className="bb-breadcrumb">
+                    <ul>
+                      <li className="breadcrumb-item active">
+                        <a href="#example">Carnivorans</a>
+                      </li>
+                      <li className="breadcrumb-item">
+                        <a href="#example">Ursidae</a>
+                      </li>
+                      <li className="breadcrumb-item">
+                        <a href="#example">Panda</a>
+                      </li>
+                    </ul>
+                  </nav>
+                  <nav className="bb-breadcrumb lg">
+                    <ul>
+                      <li className="breadcrumb-item active">
+                        <a href="#example">Carnivorans</a>
+                      </li>
+                      <li className="breadcrumb-item">
+                        <a href="#example">Ursidae</a>
+                      </li>
+                      <li className="breadcrumb-item">
+                        <a href="#example">Panda</a>
+                      </li>
+                    </ul>
+                  </nav>
+                  <nav className="bb-breadcrumb xl">
+                    <ul>
+                      <li className="breadcrumb-item active">
+                        <a href="#example">Carnivorans</a>
+                      </li>
+                      <li className="breadcrumb-item">
+                        <a href="#example">Ursidae</a>
+                      </li>
+                      <li className="breadcrumb-item">
+                        <a href="#example">Panda</a>
+                      </li>
+                    </ul>
+                  </nav>
                 </div>
                 <div className="doc-snippet">
                   <Highlight className="html">
-                    {`<div className="bb-box invert">
-  <h4 className="title">
-    The giant panda is actually a bear.
-  </h4>
-  <p className="subtitle">
-    The giant panda also known as panda bear or simply
-    panda, is a bear native to south central China.
-  </p>
-</div>`}
+                    {`<nav class="bb-breadcrumb">
+  <ul>
+    <li class="breadcrumb-item active">
+      <a href="#example">Carnivorans</a>
+    </li>
+    <li class="breadcrumb-item">
+      <a href="#example">Ursidae</a>
+    </li>
+    <li class="breadcrumb-item">
+      <a href="#example">Panda</a>
+    </li>
+  </ul>
+</nav>`}
                   </Highlight>
                   <Highlight className="html">
-                    {`<div className="bb-box primary">
-  ...
-</div>`}
+                    {`<nav className="bb-breadcrumb xs">
+  <ul>
+    <li class="breadcrumb-item active">
+      <a href="#example">Carnivorans</a>
+    </li>
+    <li class="breadcrumb-item">
+      <a href="#example">Ursidae</a>
+    </li>
+    <li class="breadcrumb-item">
+      <a href="#example">Panda</a>
+    </li>
+  </ul>
+</nav>`}
                   </Highlight>
                   <Highlight className="html">
-                    {`<div className="bb-box secondary">
+                    {`<nav class="bb-breadcrumb sm">
   ...
-</div>`}
+</nav>`}
                   </Highlight>
                   <Highlight className="html">
-                    {`<div className="bb-box positive">
+                    {`<nav class="bb-breadcrumb">
   ...
-</div>`}
+</nav>`}
                   </Highlight>
                   <Highlight className="html">
-                    {`<div className="bb-box negative">
+                    {`<nav class="bb-breadcrumb lg">
   ...
-</div>`}
+</nav>`}
+                  </Highlight>
+                  <Highlight className="html">
+                    {`<nav class="bb-breadcrumb xl">
+  ...
+</nav>`}
                   </Highlight>
                   <button className="bb-btn primary doc-copy sm">Copy</button>
                 </div>
@@ -123,66 +186,56 @@ export default class BreadcrumbDocumentation extends Component {
           </div>
           <div className="bb-grid md">
             <div className="column column-16">
-              <h4 className="title">Styles</h4>
+              <h4 className="title">Scroll</h4>
               <div className="bb-box example">
                 <div className="doc-preview">
-                  <div className="column column-10">
-                    <div className="bb-box outline">
-                      <h4>The giant panda is actually a bear.</h4>
-                    </div>
-                  </div>
-                  <div className="column column-10">
-                    <div className="bb-box outline primary">
-                      <h4>The giant panda is actually a bear.</h4>
-                    </div>
-                  </div>
-                  <div className="column column-10">
-                    <div className="bb-box outline secondary">
-                      <h4>The giant panda is actually a bear.</h4>
-                    </div>
-                  </div>
-                  <div className="column column-10">
-                    <div className="bb-box outline positive">
-                      <h4>The giant panda is actually a bear.</h4>
-                    </div>
-                  </div>
-                  <div className="column column-10">
-                    <div className="bb-box outline negative">
-                      <h4>The giant panda is actually a bear.</h4>
-                    </div>
-                  </div>
+                  <nav className="bb-breadcrumb scroll">
+                    <ul>
+                      <li className="breadcrumb-item active">
+                        <a href="#example">Carnivorans</a>
+                      </li>
+                      <li className="breadcrumb-item">
+                        <a href="#example">Ursidae</a>
+                      </li>
+                      <li className="breadcrumb-item">
+                        <a href="#example">Panda</a>
+                      </li>
+                      <li className="breadcrumb-item">
+                        <a href="#example">Brown Bear</a>
+                      </li>
+                      <li className="breadcrumb-item">
+                        <a href="#example">Polar Bear</a>
+                      </li>
+                      <li className="breadcrumb-item">
+                        <a href="#example">Sloth Bear</a>
+                      </li>
+                    </ul>
+                  </nav>
                 </div>
                 <div className="doc-snippet">
                   <Highlight className="html">
-                    {`<div className="bb-box outline">
-  <h4 className="title">
-    The giant panda is actually a bear.
-  </h4>
-  <p className="subtitle">
-    The giant panda also known as panda bear or simply
-    panda, is a bear native to south central China.
-  </p>
-</div>`}
-                  </Highlight>
-                  <Highlight className="html">
-                    {`<div className="bb-box outline primary">
-  ...
-</div>`}
-                  </Highlight>
-                  <Highlight className="html">
-                    {`<div className="bb-box outline secondary">
-  ...
-</div>`}
-                  </Highlight>
-                  <Highlight className="html">
-                    {`<div className="bb-box outline positive">
-  ...
-</div>`}
-                  </Highlight>
-                  <Highlight className="html">
-                    {`<div className="bb-box outline negative">
-  ...
-</div>`}
+                    {`<nav class="bb-breadcrumb scroll">
+  <ul>
+    <li class="breadcrumb-item active">
+      <a href="#example">Carnivorans</a>
+    </li>
+    <li class="breadcrumb-item">
+      <a href="#example">Ursidae</a>
+    </li>
+    <li class="breadcrumb-item">
+      <a href="#example">Panda</a>
+    </li>
+    <li class="breadcrumb-item">
+      <a href="#example">Brown Bear</a>
+    </li>
+    <li class="breadcrumb-item">
+      <a href="#example">Polar Bear</a>
+    </li>
+    <li class="breadcrumb-item">
+      <a href="#example">Sloth Bear</a>
+    </li>
+  </ul>
+</nav>`}
                   </Highlight>
                   <button className="bb-btn primary doc-copy sm">Copy</button>
                 </div>
